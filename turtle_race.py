b2b505bc57907b98ded6cee2bbeaae5276eebc8a
Julie K <juliette.nada@gmail.com>
2019-03-11 17:23:03 +0100

    chapter 7 turtle race setup modified with larger intervals, a backgroud pic, penup-pendown

--- turtle_race.py
+++ turtle_race.py
@@ -6,18 +6,37 @@ turtles = []
 
 def setup():
     global turtles
-    startline = -480
+    startline = -620
+    screen = turtle.Screen()
+    screen.setup(1290,720)
+    # the above setup had seemed to be too much repetation
+    # which is not true coz there are two setups to do with Screen
+    # btw tried to append the dots together like
+    # turtle.Screen.setup(1290, 720)
+    # AttributeError: 'function' object has no attribute 'setup'
+    screen.bgpic('pavement.gif')
 
-    turtle_ycor = [-40, -20, 0, 20, 40]
-    turtle_color = ['red', 'yellow', 'green', 'bleu', 'pink']
+    turtle_ycor = [160, 80, 0, -80, -160]
+    turtle_color = ['red', 'orange', 'green', 'blue', 'pink']
 
     for i in range(len(turtle_ycor)):
         new_turtle = turtle.Turtle()
         new_turtle.shape('turtle')
         new_turtle.color(turtle_color[i])
+        new_turtle.penup()
         new_turtle.setpos(startline, turtle_ycor[i])
+        new_turtle.pendown()
         turtles.append(new_turtle)
 
 
 setup()
-turtle.mainloop
+
+
+# when using turtle.mainloop()
+# window closes as soon as prog complets
+# found the following on Stackoverlow
+# another prob:
+# window smaller that display orange
+# if dragged to appropriated width
+# comes back to defaut next time
+turtle.getscreen()._root.mainloop()
