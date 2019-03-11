d9da679cf68f073a458f160801966fe1a3dcfc93
Julie K <juliette.nada@gmail.com>
2019-03-11 19:55:30 +0100

    chapter 7 race, version simple clean, version complique a faire

---
+++ 7-race_with_super_tbd.py
@@ -0,0 +1,62 @@
+import turtle
+import random
+
+turtles = []
+
+
+def setup():
+    global turtles
+    startline = -620
+    screen = turtle.Screen()
+    screen.setup(1290,720)
+    # the above setup had seemed to be too much repetation
+    # which is not true coz there are two setups to do with Screen
+    # btw tried to append the dots together like
+    # turtle.Screen.setup(1290, 720)
+    # AttributeError: 'function' object has no attribute 'setup'
+    screen.bgpic('pavement.gif')
+
+    turtle_ycor = [160, 80, 0, -80, -160]
+    turtle_color = ['red', 'orange', 'green', 'blue', 'pink']
+
+    for i in range(len(turtle_ycor)):
+        new_turtle = turtle.Turtle()
+        new_turtle.shape('turtle')
+        new_turtle.color(turtle_color[i])
+        new_turtle.penup()
+        new_turtle.setpos(startline, turtle_ycor[i])
+        new_turtle.pendown()
+        turtles.append(new_turtle)
+
+
+def race():
+    global turtles
+    winner = False
+    finishline = 590
+
+    while winner == False:
+        for current_turtle in turtles:
+            move = random.randint(5,20)
+            current_turtle.forward(move)
+            xcor = current_turtle.xcor()
+            if (xcor >= finishline):
+                winner = True
+                winner_color = current_turtle.color()
+                print('The winner is', winner_color[0], 'turtle!')
+                # winner_color[0] clearly is the first item in a list
+                # but how did winner_color become a list???
+                # I though current_turtle.color() was a defaut attribute value
+                # tried winner_color, wouldn't work
+
+
+setup()
+race()
+
+# when using turtle.mainloop()
+# window closes as soon as prog complets
+# found the following on Stackoverlow
+# another prob:
+# window smaller that display orange
+# if dragged to appropriated width
+# comes back to defaut next time
+turtle.getscreen()._root.mainloop()
