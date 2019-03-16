e9108afd45836d1e1c6559c348868a6893e193a4
GUO Junyue <juliette.nada@gmail.com>
2019-03-16 20:26:53 +0100

    chapter 8 dictionnary started

--- 8-test_dic.py
+++ 8-test_dic.py
@@ -1,4 +1,3 @@
-
 # first impression on dictionnary
 # 1, dictionnaries use square brackets instead of round brackests for keys
 # 2, lists can't be keys
@@ -11,10 +10,33 @@ mydict['type'] = 'chat'
 mydict['couleur'] = ['noir', 'blanc']
 mydict['poid'] = 6.5
 
-print('a dictionnary prints like this:')
-print(mydict)
+# print('a dictionnary prints like this:')
+# print(mydict)
 
-if 'Bateau' in mydict:
-    print('OK to use value with IN operator.')
-else:
-    print('Only key can be used for IN operator, but not value.')
+# if 'Bateau' in mydict:
+#    print('OK to use value with IN operator.')
+# else:
+#    print('Only key can be used for IN operator, but not value.')
+
+# a dictionnary of dictionnaries as a table
+users = {}
+users['Kim'] = {
+  'email': 'kim@reilly.com',
+  'gender': 'F', 'age': 27, 'friends': ['John', 'Josh']}
+users['John'] = {
+  'email': 'john@abc.com',
+  'gender': 'M', 'age': 24, 'friends': ['Kim', 'Josh']}
+users['Josh'] = {
+  'email': 'josh@wickedlysmart.com',
+  'gender': 'M', 'age': 32, 'friends': ['John', 'Kim']}
+
+print(users)
+
+# for user in users:
+#    print(user)
+#    print(users[user]['friends'])
+
+
+for user in users:
+    print(user)
+    print(users[user]['age'])
