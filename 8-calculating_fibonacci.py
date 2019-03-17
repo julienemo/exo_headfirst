e52ecea09e9f6d0d13832e0b02c6d4ab29e77f02
GUO Junyue <juliette.nada@gmail.com>
2019-03-17 19:43:22 +0100

    chapter 8 recursive fibs refactored

--- 8-calculating_fibonacci.py
+++ 8-calculating_fibonacci.py
@@ -1,18 +1,22 @@
 import time
+fibs = {}
+fibs[0] = 0
+fibs[1] = 1
 
 
-def fibonacci(pos):
-    if pos == 0:
-        return 0
-    elif pos == 1:
-        return 1
+def get_pos_fib(pos):
+    global fibs
+    if pos in fibs:
+        result = fibs[pos]
     else:
-        return fibonacci(pos-2)+fibonacci(pos-1)
+        result = get_pos_fib(pos - 1) + get_pos_fib(pos - 2)
+    fibs[pos] = result
+    return result
 
-
-for i in range(20, 55, 5):
-    start = time.time()
-    result = fibonacci(i)
-    end = time.time()
-    duration = end - start
-    print(i, ' --- ', result, ' --- ', duration)
+start = time.time()
+for i in range(100):
+    result = get_pos_fib(i)
+    print(i, ' --- ', result)
+end = time.time()
+duration = end - start
+print('All calculated in', duration, 'secs.' )
