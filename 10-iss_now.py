3e367c73e5270263cef895b578287a341f2f530b
Julie K <juliette.nada@gmail.com>
2019-03-20 16:02:21 +0100

    chapter 10 API, ISS position decoded with JSON

--- 10-iss_now.py
+++ 10-iss_now.py
@@ -1,8 +1,11 @@
 import requests
+import json
 
-url = 'http://api.cinepass.de/v3'
+url = 'http://api.open-notify.org/iss-now.json'
 reponse = requests.get(url)
 if (reponse.status_code) == 200:
-    print(reponse.text)
+    content = json.loads(reponse.text)
+    print('International Space Station position :')
+    print(content['iss_position']['latitude'], ',', content['iss_position']['longitude'])
 else:
-    print('Huston, we have a problem.', reponse.status_code)
+    print('Oups Huston, we have a problem,', reponse.status_code)
