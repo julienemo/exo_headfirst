6441a2ba4caf1f6d4ce7f9b4d05f4d2a3b1ee8e7
Julie K <juliette.nada@gmail.com>
2019-03-27 12:03:10 +0100

    chapter 10 API star wars characters printed out. wip

--- 10-star_wars.py
+++ 10-star_wars.py
@@ -10,31 +10,46 @@ def get_player_choice():
     n = input(prompt)
     while True:
         try:
-            while (int(n-1) < 0) or (int(n-1) > 87):
+            while (int(n)-1 < 0) or (int(n)-1 > 87):
                 n = input(prompt)
             break
         except:
             n = input(prompt)
-    print(int(n))
+    return n
 
 
-def get_pc_choice():
+def get_pc_choice(m):
     n = random.randint(0, 87)
-    while n == get_player_choice():
+    while n == m:
         n = random.randint(0, 87)
-    print(n)
+    return str(n)
 
 
-get_pc_choice()
-
-
+def get_url(n):
+    pre = "https://swapi.co/api/people/"
+    nb = str(n)
+    url = pre+nb+"/"
+    return url
 
 
 def get_reponse(n):
-    string = "https://swapi.co/api/people/"+str(n)+"/"
-    url = string
-    reponse = requests.get(url)
+    reponse = requests.get(n)
     if (reponse.status_code) == 200:
         return json.loads(reponse.text)
     else:
         return reponse.status_code
+
+
+player_choice = get_player_choice()
+pc_choice = get_pc_choice(player_choice)
+player_url = get_url(player_choice)
+pc_url = get_url(pc_choice)
+player_char = get_reponse(player_url)
+pc_char = get_reponse(pc_url)
+print('-----')
+
+print('-----')
+print('YOUR CHARACATER')
+print('NAME :',)
+
+print("-----")
