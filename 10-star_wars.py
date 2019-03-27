330fb0b9a64070d2863775eb8172996eb9c9036d
Julie K <juliette.nada@gmail.com>
2019-03-27 17:50:31 +0100

    chapter 10 API star war compare char common movie done. TBD calc 'who share the screen most often with your char' and 'who has the most co-star'

--- 10-star_wars.py
+++ 10-star_wars.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import random
-import turtle
+# import turtle
 import time
 
 
@@ -19,9 +19,9 @@ def get_player_choice():
 
 
 def get_pc_choice(m):
-    n = random.randint(0, 87)
+    n = random.randint(1, 88)
     while n == m:
-        n = random.randint(0, 87)
+        n = random.randint(1, 88)
     return str(n)
 
 
@@ -60,24 +60,31 @@ def count_common(n, m):
     if len(common) > 1:
         string = 'appeared together in ' + str(len(common))+' films.'
     elif len(common) == 1:
-        string = 'appeared together once in', common[0]
+        string = 'appeared together once in ' + common[0]
     else:
         string = "don't have common movie appearance."
     return string
 
 
+t0 = time.time()
 player_choice = get_player_choice()
 pc_choice = get_pc_choice(player_choice)
+t1 = time.time()
 player_url = get_url(player_choice)
 pc_url = get_url(pc_choice)
+t2 = time.time()
 player_char = get_reponse(player_url)
 pc_char = get_reponse(pc_url)
+t3 = time.time()
 player_char_hw = get_reponse(player_char['homeworld'])
 pc_char_hw = get_reponse(pc_char['homeworld'])
-player_char_fm = player_char['films']
-pc_char_fm = pc_char['films']
-string_to_print = count_common(get_films(player_char_fm), get_films(pc_char_fm))
+t4 = time.time()
+player_char_fm_content = player_char['films']
+pc_char_fm_content = pc_char['films']
+player_fm_name_list = get_films(player_char_fm_content)
+pc_fm_name_list = get_films(pc_char_fm_content)
 
+t6 = time.time()
 print('-----')
 print('---YOUR CHARACTER---')
 print('CHARACTER N.', player_choice)
@@ -85,8 +92,10 @@ print('NAME:', player_char['name'])
 print('BIRTH YEAR:', player_char['birth_year'])
 print('HOMEWORLD:', player_char_hw['name'])
 print('HEIGHT:', player_char['height'])
-print('MOVIE APPREARANCE:')
-print_films(get_films(player_char_fm))
+t7 = time.time()
+print('MOVIE APPREARANCE:', len(player_fm_name_list))
+print_films(player_fm_name_list)
+t8 = time.time()
 
 print('')
 print('---PC CHARACATER---')
@@ -95,9 +104,23 @@ print('NAME:', pc_char['name'])
 print('BIRTH YEAR:', pc_char['birth_year'])
 print('HOMEWORLD:', pc_char_hw['name'])
 print('HEIGHT:', pc_char['height'])
-print('MOVIE APPREARANCE:')
-print_films(get_films(pc_char_fm))
+t9 = time.time()
+print('MOVIE APPREARANCE:', len(pc_fm_name_list))
+print_films(pc_fm_name_list)
+t10 = time.time()
 print("")
 print("-----")
 print(player_char['name'], 'and', pc_char['name'])
-print(string_to_print)
+print(count_common(player_fm_name_list, pc_fm_name_list))
+t12 = time.time()
+print('')
+print('---CHRONO---')
+print('Getting choices:', str(round((t1-t0), 4)) + 'sec')
+print('Composing URLs:', str(round((t2-t1), 4)) + 'sec')
+print('Reaching API for characters:', str(round((t3-t2), 4)) + 'sec')
+print('Printing character info:', str(round((t7+t9-t6-t8), 4)) + 'sec')
+print('Reaching API for homeworlds:', str(round((t4-t3), 4)) + 'sec')
+print('Reaching API for movies:', str(round((t6-t4), 4)) + 'sec')
+print('Printing movies lists:', str(round((t6+t8+t10-t4-t7-t9), 4)) + 'sec')
+print('Comparing movie lists:', str(round((t7-t6), 4)) + 'sec')
+print('Total time:', str(round((t7-t0), 4)) + 'sec')
